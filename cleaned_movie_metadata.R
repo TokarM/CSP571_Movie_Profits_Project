50fc41b5742b8d47d54cb8280ba0feae4ff1bc7f
AK Ho <kayinho@dhcp60.merusouth126.iit.edu>
2020-02-29 16:11:41 -0600

    push the cleaned movie metadata

---
+++ cleaned_movie_metadata.R
@@ -0,0 +1,27 @@
+install.packages('tidyr')
+library('tidyr')
+movieMaster <- read.csv('/Users/kayinho/Documents/IIT/Sem2 - 2020Spring/CSP571/git_project/movie_forecasting/CSP571_Movie_Profits_Project/the-movies-dataset/movies_metadata.csv'
+              ,header = T, sep = ",")
+movieMaster <- movieMaster[,c('id','budget','genres','original_title','production_companies','release_date','revenue', 'runtime')]
+
+movieMaster$release_date <- as.Date(movieMaster$release_date)
+movieMaster <- movieMaster[(movieMaster$release_date > "2000-01-01"),]
+movieMaster <- movieMaster[(movieMaster$release_date < "2017-12-31"),]
+
+movieMaster <- separate(movieMaster, genres, c("start","genre1"), "'name': '")
+movieMaster <- separate(movieMaster, genre1, c("1st genre"), "'")
+movieMaster <- movieMaster[,c('id','budget','1st genre','original_title','production_companies','release_date','revenue', 'runtime')]
+movieMaster <- separate(movieMaster, 'production_companies', c("start","producation comp","others"), "'name': '")
+movieMaster <- separate(movieMaster, 'producation comp', c('1st production company'), "', 'id': ")
+movieMaster <- movieMaster[,c('id','budget','1st genre','original_title','1st production company','release_date','revenue', 'runtime')]
+
+movieMaster$`1st genre` <- tolower(movieMaster$`1st genre`)
+movieMaster$original_title <- tolower(movieMaster$original_title)
+movieMaster$`1st production company` <- tolower(movieMaster$`1st production company`)
+
+movieMaster <- separate(movieMaster, release_date, c('Year', 'Month', 'Date')
+         , sep='-'
+         , remove=FALSE)
+
+head(movieMaster, n = 100)
+
