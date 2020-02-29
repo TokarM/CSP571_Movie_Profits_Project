81c58fc40cbb62e8a71f909f20fa78a7c5aace57
AK Ho <kayinho@dhcp60.merusouth126.iit.edu>
2020-02-29 16:40:00 -0600

    deleted punc of movie title

--- cleaned_movie_metadata.R
+++ cleaned_movie_metadata.R
@@ -7,7 +7,7 @@ movieMaster <- movieMaster[,c('id','budget','genres','original_title','productio
 movieMaster$release_date <- as.Date(movieMaster$release_date)
 movieMaster <- movieMaster[(movieMaster$release_date > "2000-01-01"),]
 movieMaster <- movieMaster[(movieMaster$release_date < "2017-12-31"),]
-
+head(movieMaster, n= 50)
 movieMaster <- separate(movieMaster, genres, c("start","genre1"), "'name': '")
 movieMaster <- separate(movieMaster, genre1, c("1st genre"), "'")
 movieMaster <- movieMaster[,c('id','budget','1st genre','original_title','production_companies','release_date','revenue', 'runtime')]
@@ -17,11 +17,14 @@ movieMaster <- movieMaster[,c('id','budget','1st genre','original_title','1st pr
 
 movieMaster$`1st genre` <- tolower(movieMaster$`1st genre`)
 movieMaster$original_title <- tolower(movieMaster$original_title)
+movieMaster$original_title<-gsub('[[:punct:] ]+',' ',movieMaster$original_title)
 movieMaster$`1st production company` <- tolower(movieMaster$`1st production company`)
 
 movieMaster <- separate(movieMaster, release_date, c('Year', 'Month', 'Date')
          , sep='-'
          , remove=FALSE)
+
+write.csv(movieMaster, file = "/Users/kayinho/Documents/IIT/Sem2 - 2020Spring/CSP571/git_project/movie_forecasting/CSP571_Movie_Profits_Project/the-movies-dataset/cleaned_movies_metadata.csv")
 
 head(movieMaster, n = 100)
 
