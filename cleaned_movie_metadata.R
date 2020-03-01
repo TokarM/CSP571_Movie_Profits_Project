d2af19e58950b06b3e3273da1339a976152cea42
AK Ho <kho5@hawk.iit.edu>
2020-02-29 21:29:15 -0600

    added cleaned_movies_metadata_1.csv (right duration, cleared punc. and white space for movie title)

--- cleaned_movie_metadata.R
+++ cleaned_movie_metadata.R
@@ -5,10 +5,10 @@ movieMaster <- read.csv('/Users/kayinho/Documents/IIT/Sem2 - 2020Spring/CSP571/g
 movieMaster <- movieMaster[,c('id','budget','genres','original_title','production_companies','release_date','revenue', 'runtime')]
 
 movieMaster$release_date <- as.Date(movieMaster$release_date)
-movieMaster <- movieMaster[(movieMaster$release_date > "2000-01-01"),]
-movieMaster <- movieMaster[(movieMaster$release_date < "2017-12-31"),]
+movieMaster <- movieMaster[(movieMaster$release_date > "1999-12-31"),]
+movieMaster <- movieMaster[(movieMaster$release_date < "2018-01-01"),]
 head(movieMaster, n= 50)
-movieMaster <- separate(movieMaster, genres, c("start","genre1"), "'name': '")
+movieMaster <- separate(movieMaster, genres, c('start','genre1'), "'name': '")
 movieMaster <- separate(movieMaster, genre1, c("1st genre"), "'")
 movieMaster <- movieMaster[,c('id','budget','1st genre','original_title','production_companies','release_date','revenue', 'runtime')]
 movieMaster <- separate(movieMaster, 'production_companies', c("start","producation comp","others"), "'name': '")
@@ -18,13 +18,15 @@ movieMaster <- movieMaster[,c('id','budget','1st genre','original_title','1st pr
 movieMaster$`1st genre` <- tolower(movieMaster$`1st genre`)
 movieMaster$original_title <- tolower(movieMaster$original_title)
 movieMaster$original_title<-gsub('[[:punct:] ]+',' ',movieMaster$original_title)
-movieMaster$`1st production company` <- tolower(movieMaster$`1st production company`)
+movieMaster['cleaned title'] <- gsub(' ', '', movieMaster$original_title)
+#movieMaster$`1st production company` <- tolower(movieMaster$`1st production company`)
+head(movieMaster, n = 50)
 
 movieMaster <- separate(movieMaster, release_date, c('Year', 'Month', 'Date')
          , sep='-'
          , remove=FALSE)
 
-write.csv(movieMaster, file = "/Users/kayinho/Documents/IIT/Sem2 - 2020Spring/CSP571/git_project/movie_forecasting/CSP571_Movie_Profits_Project/the-movies-dataset/cleaned_movies_metadata.csv")
+write.csv(movieMaster, file = "cleaned_movies_metadata_1.csv")
 
 head(movieMaster, n = 100)
 
