c5f40c50120e0a5fd924aa4e6a7d4cad2eae399b
TokarM <mykolatokar1995@gmail.com>
2020-04-17 18:51:57 -0500

    Removing outliers R file

--- clean_outliers_final_dataset.R
+++ clean_outliers_final_dataset.R
@@ -1,4 +1,4 @@
-movie <- read.csv('/Users/nick/Desktop/CSP571_Movie_Profits_Project/final_dataset.csv', header = TRUE, stringsAsFactors = FALSE)
+movie <- read.csv('/Users/nick/Desktop/CSP571_Movie_Profits_Project/5_merged_with_holidays.csv', header = TRUE, stringsAsFactors = FALSE)
 head(movie)
 
 #Delete runtime outliers
@@ -6,15 +6,27 @@ boxplot(movie$runtime)
 id1 <- boxplot.stats(movie$runtime, 2)
 id1$stats
 
-movie <- movie[which(movie$runtime < 157),]
-movie <- movie[which(movie$runtime > 58),]
+movie <- movie[which(movie$runtime < 163),]
+movie <- movie[which(movie$runtime > 53),]
 
-
-boxplot(movie$ProductionBudget)
-id2 <- boxplot.stats(movie$ProductionBudget, 2)
+boxplot(movie$budget)
+id2 <- boxplot.stats(movie$budget, 2)
 id2$stats
 
-movie <- movie[which(movie$ProductionBudget < 103000000),]
-movie <- movie[which(movie$ProductionBudget > 7000),]
+movie <- movie[which(movie$budget < 57000000),]
+movie <- movie[which(movie$budget > 1),]
 
-write.csv(movie, '/Users/nick/Desktop/CSP571_Movie_Profits_Project/final_dataset.csv')
+#movie['EarningPercent'] <- (movie$revenue - movie$budget) / movie$budget
+
+movie$X <- NULL
+movie$id <- NULL
+movie$release_date <- NULL
+movie$Actor <- NULL
+movie$Director <- NULL
+movie$revenue <- NULL
+movie$veteransdayobserved <- NULL
+movie$related_holiday <- NULL
+
+write.csv(movie, '/Users/nick/Desktop/CSP571_Movie_Profits_Project/no_outliers_final_dataset.csv')
+
+
