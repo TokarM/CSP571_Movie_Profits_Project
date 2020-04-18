9f20597dada383c11cb9d08e240c0a6553ba6b51
TokarM <mykolatokar1995@gmail.com>
2020-04-17 21:03:16 -0500

    Pushdataset with no outliers and only important features

--- clean_outliers_final_dataset.R
+++ clean_outliers_final_dataset.R
@@ -1,19 +1,18 @@
 movie <- read.csv('/Users/nick/Desktop/CSP571_Movie_Profits_Project/5_merged_with_holidays.csv', header = TRUE, stringsAsFactors = FALSE)
 head(movie)
 
-#Delete runtime outliers
 boxplot(movie$runtime)
-id1 <- boxplot.stats(movie$runtime, 1.5)
+id1 <- boxplot.stats(movie$runtime, 2)
 id1$stats
 
-movie <- movie[which(movie$runtime < 151),]
-movie <- movie[which(movie$runtime > 62),]
+movie <- movie[which(movie$runtime < 163),]
+movie <- movie[which(movie$runtime > 53),]
 
 boxplot(movie$budget)
-id2 <- boxplot.stats(movie$budget, 1.5)
+id2 <- boxplot.stats(movie$budget, 2.0)
 id2$stats
 
-movie <- movie[which(movie$budget < 48000000),]
+movie <- movie[which(movie$budget < 57000000),]
 movie <- movie[which(movie$budget > 1),]
 
 #movie['EarningPercent'] <- (movie$revenue - movie$budget) / movie$budget
@@ -25,26 +24,8 @@ movie$release_date <- NULL
 movie$Actor <- NULL
 movie$Director <- NULL
 movie$revenue <- NULL
-movie$veteransdayobserved <- NULL
 movie$related_holiday <- NULL
-movie$christmasdayobserved <- NULL
-movie$independencedayobserved <- NULL
-movie$columbusday <- NULL
-movie$crismaseve <- NULL
-movie$mystery <- NULL
-movie$martinlutherkingjrday <- NULL
-movie$goodfriday <- NULL
-movie$fathersday <- NULL
-movie$christmaseve <- NULL
-movie$valentinesday <- NULL
-movie$veteransday <- NULL
-movie$numOfHolidays <- NULL
-movie$animation <- NULL
-movie$comedy <- NULL
-movie$war <- NULL
-movie$christmaseveobserved <- NULL
-movie$presidentsday <- NULL
-movie$laborday <- NULL
+
 
 write.csv(movie, '/Users/nick/Desktop/CSP571_Movie_Profits_Project/no_outliers_final_dataset.csv')
 
---
+++ logistic_regression_v3.R
@@ -0,0 +1,118 @@
+#Reading data from csv file
+movie <- read.csv('/Users/nick/Desktop/CSP571_Movie_Profits_Project/logistic_final_dataset.csv', header = TRUE, stringsAsFactors = FALSE)
+head(movie)
+movie$X <- NULL
+movie$X.1 <- NULL
+#movie$ScaledProductionBudget <- scale(movie$ProductionBudget)
+
+#Ranking the quarter variable 
+set.seed(123)
+library('caret')
+library('tidyverse')
+
+#Scaling
+df_continuous <- movie[c("runtime", "budget")]
+df_scaled_cont <- scale(df_continuous) 
+movie[c("runtime", "budget")] <- df_scaled_cont
+
+
+#We'll do stratified sampling to split our data into training and test sets
+targetVar <- 'success_1_to_1'
+
+inTrain <- createDataPartition(y = movie[,targetVar], list = FALSE, p = .8)
+trainData <- movie[inTrain,]
+testData <- movie[-inTrain,]
+stopifnot(nrow(trainData) + nrow(testData) == nrow(movie))
+
+library(MASS)
+
+#Logistic Regression on train dataset
+#logitModel <- glm(success_1_to_1 ~ .,family=binomial(link='logit'),data=trainData) %>% stepAIC(trace=FALSE)
+logitModel <- glm(success_1_to_1 ~ .,family=binomial(link='logit'),data=trainData)
+summary(logitModel)
+
+#Predict on test data
+predicted <- predict(logitModel, testData, type="response")
+
+#Histogram of prediction
+hist(predicted)
+
+#testData[,'predicted'] <- predicted
+
+#Deciding optimal prediction probability cutoff for the model
+install.packages("InformationValue")
+library(InformationValue)
+optCutOff <- optimalCutoff(testData$success_1_to_1, predicted)[1] 
+print(optCutOff)
+#Model Diagnostics
+summary(logitModel)
+
+#Classification of Success or not.
+success.pred <- ifelse(predicted > 0.5,1,0)
+
+optCutOff.success.pred <- ifelse(predicted > optCutOff,1,0)
+
+#Claculating the mean of prediction
+mean(success.pred)
+mean(optCutOff.success.pred)
+
+# Checking classification accuracy
+mean(success.pred == testData$success_1_to_1)
+
+mean(optCutOff.success.pred == testData$success_1_to_1)  
+
+#Confusion matrix to evaluate how good our results are
+check_acc <- as.data.frame(as.factor(testData$success_1_to_1))
+check_acc['actual'] <- as.data.frame(as.factor(testData$success_1_to_1))
+check_acc['prediction'] <- as.factor(success.pred)
+caret::confusionMatrix(data=check_acc$prediction, reference=check_acc$actual)
+
+check_acc['opt_prediction'] <- as.factor(optCutOff.success.pred)
+caret::confusionMatrix(data=check_acc$opt_prediction, reference=check_acc$actual)
+
+
+#Missclassification Error
+misClassError(testData$success_1_to_1, predicted, threshold = optCutOff)
+
+#If we take optcutoff/thershold as 0.5
+misClassError(testData$success_1_to_1, predicted, threshold = 0.5)
+
+#The ROC curve
+plotROC(testData$success_1_to_1, predicted)
+
+#install.packages("ROCR")
+#library(ROCR)
+#pr <- prediction(predicted, testData$Success_2_to_1)
+#prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+#plot(prf)
+
+#auc <- performance(pr, measure = "auc")
+#auc <- auc@y.values[[1]]
+#auc
+
+#Calcuting Concordance
+Concordance(testData$success_1_to_1, predicted)
+
+#Ploting precision recall curves
+install.packages('DMwR')
+library('DMwR')
+PRcurve(preds = predicted, trues = testData$success_1_to_1)
+
+#The deviance
+llcomponents <- function(y, predicted.y){
+  return(y*log(predicted.y) + (1-y)*log(1-predicted.y))
+}
+
+xVars <- c( 'runtime', 'ProductionBudget', 'drama', 'action', 'amusement')
+y <- trainData[,targetVar]
+predicted.y <- predict(logitModel, newdata = trainData[,xVars], type='response')
+
+deviance <- sign(as.numeric(y) - predicted.y)*sqrt(-2*llcomponents(as.numeric(y), predicted.y))
+
+summary(deviance)
+
+# Extract the AIC
+aic<- 2 * length(logitModel$coefficients) - 2*logLik(logitModel)
+aic
+AIC(logitModel)
+
